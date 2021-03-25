00a23f2683efdde368e094d9d378427b53e552ab
mmi41 <mmi41@mail.aub.edu>
2021-03-25 03:02:44 +0200

    hamed sucks dick part 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('hello'),
+          child: Text('hamed sucks dick'),
         ),
       ),
     );
