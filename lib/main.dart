f9a3129559bb86ec96af7d7bca4836a55cff6412
Karim Itani <kii00@mail.aub.edu>
2021-03-28 23:13:56 +0300

    revert

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:proj21/homepage.dart';
 import 'package:proj21/merchantregistration.dart';
-import 'package:proj21/driverregistration.dart';
-import 'package:proj21/loginscreen.dart';
-import 'package:proj21/otp.dart';
+import 'package:proj21/DriverRegistration.dart';
+
 
 void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
-  // Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(new MyApp());
 }
 
